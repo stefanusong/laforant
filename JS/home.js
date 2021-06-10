4c9498b77a255892635d1992a65e23436ca59a5a
paulus224157 <paulustrianto224157@gmail.com>
2021-06-10 20:09:01 +0700

    Add: JQuery, responsive homepage, dropdown navbar

---
+++ JS/home.js
@@ -0,0 +1,24 @@
+var screen = window.matchMedia("(max-width: 900px)")
+let state = false
+
+screen.addListener(toggleNavbarNav)
+
+function toggleNavbarNav() {
+    let nav = document.getElementsByClassName('nav')[0]
+
+    if (!screen.matches) {
+        nav.style.display = 'flex'
+        state = false
+    }
+
+    else if (screen.matches) {
+        if (!state) {
+            nav.style.display = 'none'
+            state = true
+        }
+        else if (state) {
+            nav.style.display = 'block'
+            state = false
+        }
+    }
+}
