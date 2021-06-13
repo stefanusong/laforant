7fb613d5b2327e3af25245f4b43f6eddb152bdc0
Stefanus Ong <stefanusoms@gmail.com>
2021-06-13 11:42:34 +0700

    lupa pasang jquery wkwk

---
+++ JS/spec.js
@@ -0,0 +1,55 @@
+// HEADER SECTION - RESPONSIVE NAVBAR
+
+var screen = window.matchMedia("(max-width: 900px)")
+let nav = document.getElementsByClassName('nav')[0]
+let state = false
+if (screen.matches) {
+    state = true
+}   else if (!screen.matches) {
+    state = false
+}
+
+
+screen.addListener(toggleNavbarNav)
+
+function toggleNavbarNav() {
+    let nav = document.getElementsByClassName('nav')[0]
+
+    if (!screen.matches) {
+        nav.style.display = 'flex'
+        state = false
+    }
+
+    else if (screen.matches) {
+        if (!state) {
+            nav.style.display = 'none'
+            state = true
+        }
+        else if (state) {
+            nav.style.display = 'block'
+            state = false
+        }
+    }
+}
+
+$(document).ready(function(){
+
+    // HEADER SECTION - RESPONSIVE DROPDOWN
+
+    let active = false;
+
+    $(".dropdown").click(function() {
+            if (!active) {
+                $('.dropdown-menu').css({
+                display: 'block'
+                })
+                active = true;
+            }
+            else if (active) {
+                $('.dropdown-menu').css({
+                display: 'none'
+                })
+                active = false;
+            }
+    })
+});
